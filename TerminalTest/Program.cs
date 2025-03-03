11ec2cbfcce9a4e4165782918b55acf46a2739b3
bbr022025 <bbr022025@gmail.com>
2025-03-03 14:32:25 +0100

    character escape sequences, verbatim strings, unicode, and string interpolation to generate output

--- TerminalTest/Program.cs
+++ TerminalTest/Program.cs
@@ -4,12 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        string firstName = "Bob";
-        string greeting = "Hello";
-        // string message = greeting + " " + firstName + "!";
-        Console.WriteLine(greeting + " " + firstName + "!");
+        string projectName = "ACME";
 
+        string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 
+        Console.WriteLine("View English output:\n\t"
+         + $@"c:\Exercise\{projectName}\data.txt" +
+          "\n"
+          + russianMessage
+          + ":" + "\n\t"
+          + $@"c:\Exercise\{projectName}\ru-Ru\data.txt");
+        //         You may only use either the Console.WriteLine() or the Console.Write() method twice.
+        //         Use character escape sequences, verbatim strings, unicode, and string interpolation to generate the output.
+        //         View English output:
+        //     c:\Exercise\ACME\data.txt
+
+        //   Посмотреть русский вывод:
+        //   c:\Exercise\ACME\ru - RU\data.txt
     }
 }
-// Hello, Bob! You have 3 messages in your inbox. The temperature is 34.4 celsius.
--- TerminalTest/obj/Debug/net9.0/TerminalTest.AssemblyInfo.cs
+++ TerminalTest/obj/Debug/net9.0/TerminalTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TerminalTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a48a00dc16f8dd379213560face18bf24b47db0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TerminalTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TerminalTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
