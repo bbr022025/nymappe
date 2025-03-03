a48a00dc16f8dd379213560face18bf24b47db0c
bbr022025 <bbr022025@gmail.com>
2025-03-03 13:58:35 +0100

    initial commit - playing around

---
+++ TerminalTest/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TerminalTest;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string firstName = "Bob";
+        string greeting = "Hello";
+        // string message = greeting + " " + firstName + "!";
+        Console.WriteLine(greeting + " " + firstName + "!");
+
+
+    }
+}
+// Hello, Bob! You have 3 messages in your inbox. The temperature is 34.4 celsius.
