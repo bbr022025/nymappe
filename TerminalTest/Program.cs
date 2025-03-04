a601bb8e344b5cffa32039a93ac4700f1fc5db2b
bbr022025 <bbr022025@gmail.com>
2025-03-04 12:55:33 +0100

    experimenting with mathematical operations

--- TerminalTest/Program.cs
+++ TerminalTest/Program.cs
@@ -2,30 +2,41 @@
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        string projectName = "ACME";
+  static void Main(string[] args)
+  {
+    string projectName = "ACME";
+    string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 
-        string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+    Console.WriteLine("View English output:\n\t"
+     + $@"c:\Exercise\{projectName}\data.txt" +
+      "\n"
+      + russianMessage
+      + ":" + "\n\t"
+      + $@"c:\Exercise\{projectName}\ru-Ru\data.txt");
 
-        Console.WriteLine("View English output:\n\t"
-         + $@"c:\Exercise\{projectName}\data.txt" +
-          "\n"
-          + russianMessage
-          + ":" + "\n\t"
-          + $@"c:\Exercise\{projectName}\ru-Ru\data.txt");
+    //mathoperations
 
-        //mathoperations
 
-        int sum = 7 + 5;
-        int difference = 7 - 5;
-        int product = 7 * 5;
-        int quotient = 7 / 5;
+    // decimal decimalQuotient = 7.0m / 5;
+    // Console.WriteLine($"Decimal Quotient: {decimalQuotient}");
 
-        Console.Write("\n");
-        Console.WriteLine("Sum: " + sum);
-        Console.WriteLine("Difference: " + difference);
-        Console.WriteLine("Product: " + product);
-        Console.WriteLine("Quotient: " + quotient);
-    }
+    // int firstNumber = 7;
+    // int secondNumber = 5;
+    // decimal sum = firstNumber / secondNumber;
+    // Console.WriteLine($"The first sum is equal to {sum}.");
+    // decimal sum2 = (decimal)firstNumber / (decimal)secondNumber;
+    // Console.WriteLine($"The second sum is equal to {sum2}.");
+    // Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+    // Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+    //         int sum = 7 + 5;
+    //         int difference = 7 - 5;
+    //         int product = 7 * 5;
+    //         int quotient = 7 / 5;
+
+    //         Console.Write("\n");
+    //         Console.WriteLine("Sum: " + sum);
+    //         Console.WriteLine("Difference: " + difference);
+    //         Console.WriteLine("Product: " + product);
+    //         Console.WriteLine("Quotient: " + quotient);
+  }
 }
--- TerminalTest/obj/Debug/net9.0/TerminalTest.AssemblyInfo.cs
+++ TerminalTest/obj/Debug/net9.0/TerminalTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TerminalTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11ec2cbfcce9a4e4165782918b55acf46a2739b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a013e0a14fc87897c79ef31db71ffa2ffce1da59")]
 [assembly: System.Reflection.AssemblyProductAttribute("TerminalTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TerminalTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
